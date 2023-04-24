96817dfed2bd0bfb2c383eb722a2c865e4b92917
slayonce <katharinemg@gmail.com>
2023-04-23 23:09:12 -0400

    added starter variables to js file

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,19 @@
 "use strict";
+// SECTION: DOM ELEMENTS
+let formInput = $("input");
+let savedCity1 = $(".city1");
+let selectedCity = $(".selected-city");
+let selectedCityTemp = $("#selected-city-temp");
+let selectedCityWind = $("#selected-city-wind");
+let selectedCityHumid = $("selected-city-humid");
+
+const apiKey = "a7721d270b5b8958e55f066db552d8a1";
+let city;
+let queryURL =
+  "http.api.openweathermap.org/data/2.5/forecast?q=" +
+  city +
+  "&appid=" +
+  apiKey;
+
+// api.openweathermap.org/data/2.5/forecast?q={city name},{state code},{country code}&appid={API key}
+fetch(queryURL);
