ca9d4d669266460c6a60a8177b8616854b00fd0c
slayonce <katharinemg@gmail.com>
2023-04-24 18:10:00 -0400

    outlined js steps and added to readme

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # Makin-fetch-happen
-A weather 
+
+A weather dashboard
+
+## Author:
+
+Kat Gorecki
+
+### User Story:
+
+### Usage:
+
+-
+
+[Live Deployment]()
+
+### Screenshots:
+
+### References:
+
+1. The Full-Stack Bog (https://coding-boot-camp.github.io/full-stack/apis/how-to-use-api-keys)
+2. MDN (https://developer.mozilla.org/en-US/)
+3. Stack Overflow (https://stackoverflow.co/)
+4. UDEMY Jonas Schmedtmann courses (https://codingheroes.io/resources/)
+5. UDEMY Dr Angela Yu (https://appbrewery.com/)
+6. W3Schools (https://www.w3schools.com/)
+
+### License
+
+N/A
--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,14 @@
 "use strict";
 // SECTION: DOM ELEMENTS
-let formInput = $("input");
-let savedCity1 = $(".city1");
-let selectedCity = $(".selected-city");
-let selectedCityTemp = $("#selected-city-temp");
-let selectedCityWind = $("#selected-city-wind");
-let selectedCityHumid = $("selected-city-humid");
+
+// let savedCity1 = $(".city1");
+// let selectedCity = $(".selected-city");
+// let selectedCityTemp = $("#selected-city-temp");
+// let selectedCityWind = $("#selected-city-wind");
+// let selectedCityHumid = $("selected-city-humid");
+let searchForm = $("#search-form");
+let searchHistory = $(".list-group");
+let currentWeather = $("#current-weather");
 
 const apiKey = "a7721d270b5b8958e55f066db552d8a1";
 let city;
@@ -16,4 +19,17 @@ let queryURL =
   apiKey;
 
 // api.openweathermap.org/data/2.5/forecast?q={city name},{state code},{country code}&appid={API key}
-fetch(queryURL);
+// fetch(queryURL);
+
+// SECTION: ADD EVENT LISTENER - SUBMIT CITY/ZIP
+searchForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+});
+
+// SECTION: ADD FUNCTION - FETCH WEATHER FUNCTION
+
+// SECTION: ADD FUNCTION - DISPLAY CURRENT WEATHER
+
+// SECTION: ADD FUNCTION - SAVE CITIES TO SEARCH HISTORY / LOCAL STORAGE
+
+// SECTION: ADD EVENT - CLICK SAVED CITY
