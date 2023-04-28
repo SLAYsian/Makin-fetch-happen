4417bfb07d958dceafb284b444d4cec435cecec0
slayonce <katharinemg@gmail.com>
2023-04-28 00:07:19 -0400

    fixing the https issue

--- assets/script.js
+++ assets/script.js
@@ -15,7 +15,7 @@ let forecastBodyEl = document.querySelectorAll(".card-body");
 
 const apiKey = "a7721d270b5b8958e55f066db552d8a1";
 let city;
-const queryURL = `https.api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=imperial`;
+// const queryURL = `https.api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=imperial`;
 
 // api.openweathermap.org/data/2.5/forecast?q={city name},{state code},{country code}&appid={API key}
 // fetch(queryURL);
